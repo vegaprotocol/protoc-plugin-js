203f73ba1ab790e7c1bdff0fdb5ebb75c4e216a6
Emil Bay <github@tixz.dk>
2022-07-28 16:24:32 +0200

    Remove confusing method name (thanks Edd)

--- utils/join.mjs
+++ utils/join.mjs
@@ -1,4 +1,4 @@
-export default function redent(strings, ...pieces) {
+export default function(strings, ...pieces) {
   let str = strings[0]
   for (let i = 0; i < pieces.length; i++) {
     str += Array.isArray(pieces[i]) ? pieces[i].join('\n') : pieces[i]
